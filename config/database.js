1c8e2027bfa8f83205536c960aaca3f18a501337
Marcos Hirazawa <hirazawa.marcos@gmail.com>
2022-03-21 15:07:23 -0300

    database config

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'ec2-34-238-37-113.compute-1.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi1'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'Tico011099'),
+      database: env('DATABASE_NAME', 'd8p4lp0olqjifr'),
+      user: env('DATABASE_USERNAME', 'rpmxuitbbimzua'),
+      password: env('DATABASE_PASSWORD', 'bae2b6930b4a47e08c6b1d6384f8dd7bebfdf203f1f0d0477f4f0d122465fa58'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
